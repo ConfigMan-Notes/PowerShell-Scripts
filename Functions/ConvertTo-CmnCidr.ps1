453591e70c1ac467d499e75fbc9b6ac70d4e2bf5
Jim Parris <jim@parrisfamily.com>
2026-04-06 21:28:10 -0500

    Updates

--- Functions/ConvertTo-CmnCidr.ps1
+++ Functions/ConvertTo-CmnCidr.ps1
@@ -14,16 +14,22 @@ Function ConvertTo-CmnCidr {
         Subnet mask in dotted decimal notation
 
     .PARAMETER logFile
-        File for writing logs to (default is C:\Windows\Temp\Error.log).
+        File for writing logs to (default is C:\Windows\Temp\<ScriptName>.log).
 
     .PARAMETER logEntries
-        Set to $true to write to the log file. Otherwise, it will just be New-CMNLogEntry -Entry (default is $false).
+        Set to $true to write to the log file. Otherwise, it will just be write-verbose (default is $false).
+
+    .PARAMETER component
+        This is a placeholder so you can use @NewLogEntry hash table in calling scripts
 
     .PARAMETER maxLogSize
         Max size for the log (default is 5MB).
 
     .PARAMETER maxLogHistory
         Specifies the number of history log files to keep (default is 5).
+
+    .PARAMETER WriteOutput
+        Write Output to screen also. Default is $false
     
     .EXAMPLE
         ConvertTo-CmnCidr -ipAddress 192.168.0.1 -subnetMask 255.255.240.0
@@ -43,7 +49,7 @@ Function ConvertTo-CmnCidr {
  
     [CmdletBinding(ConfirmImpact = 'Low')]
     Param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = 'IP Address in dotted decimal notation')]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, HelpMessage = 'IP Address in dotted decimal notation')]
         [ValidatePattern('^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')]
         [string]$ipAddress,
         
@@ -51,43 +57,58 @@ Function ConvertTo-CmnCidr {
         [ValidatePattern('^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')]
         [String]$subnetMask,
 
-        [Parameter(Mandatory = $false, HelpMessage = 'File for writing logs to (default is C:\Windows\Temp\Error.log).')]
-        [String]$logFile = 'C:\Windows\Temp\Error.log',
+        [Parameter(Mandatory = $false, HelpMessage = 'File for writing logs to (default is C:\Windows\Temp\Dell\<ScriptName>.log).')]
+        [String]$logFile,
 
         [Parameter(Mandatory = $false, HelpMessage = 'Set to $true to write to the log file. Otherwise, it will just be write-verbose (default is $false).')]
         [Boolean]$logEntries = $false,
+
+        [Parameter(Mandatory = $false, HelpMessage = 'This is a placeholder so you can use @NewLogEntry hash table in calling scripts')]
+        [string]$component,
 
         [Parameter(Mandatory = $false, HelpMessage = 'Max size for the log (default is 5MB).')]
         [Int]$maxLogSize = 5242880,
 
         [Parameter(Mandatory = $false, HelpMessage = 'Specifies the number of history log files to keep (default is 5).')]
-        [Int]$maxLogHistory = 5
+        [Int]$maxLogHistory = 5,
+
+        [Parameter(Mandatory = $false, HelpMessage = 'Writes output to screen also. Default is $false')]
+        [boolean]$writeOutput = $false
     )
 
     begin {
-        #Build splat for log entries
-        $NewLogEntry = @{
-            logFile       = $logFile;
-            component     = 'ConvertTo-CmnCidr';
-            logEntries    = $logEntries;
-            maxLogSize    = $MaxLogSize;
-            maxLogHistory = $maxLogHistory;
+        # Ensure we have log file name
+        if ($null -eq $logFile -or $logFile -eq '') {
+            $logPath = "$($env:WinDir)\Temp\"
+            $logName = (Split-Path -Path $PSCommandPath -Leaf).Replace('ps1', 'log')
+            $logFile = "$($logPath)$($logName)"
         }
-
-        # Log variables
-        New-CMNLogEntry -entry 'Starting Function' -type 1 @NewLogEntry
-        New-CMNLogEntry -entry "ipAddress = $ipAddress" -type 1 @NewLogEntry
-        New-CMNLogEntry -entry "subnetMask = $subnetMask" -type 1 @NewLogEntry
-        New-CMNLogEntry -entry "logFile = $logFile" -type 1 @NewLogEntry
-        New-CMNLogEntry -entry "logEntries = $logEntries" -type 1 @NewLogEntry
-        New-CMNLogEntry -entry "maxLogSize = $maxLogSize" -type 1 @NewLogEntry
-        New-CMNLogEntry -entry "maxLogHistory = $maxLogHistory" -type 1 @NewLogEntry
+ 
+        # Build splat for log entries
+        $NewLogEntry = @{
+            logFile       = $logFile
+            logEntries    = $logEntries
+            component     = 'ConvertTo-CmnCidr'
+            maxLogSize    = $maxLogSize
+            maxLogHistory = $maxLogHistory
+            writeOutput   = $writeOutput
+        }
+		
+        # Log Variables
+        New-CmnLogEntry @NewLogEntry -type 1 -entry 'Starting Function'
+        New-CmnLogEntry @NewLogEntry -type 1 -entry "ipaddress     = $ipaddress"
+        New-CmnLogEntry @NewLogEntry -type 1 -entry "subnetmask    = $subnetMask"
+        New-CmnLogEntry @NewLogEntry -type 1 -entry "logFile       = $logFile"
+        New-CmnLogEntry @NewLogEntry -type 1 -entry "logEntries    = $logEntries"
+        New-CmnLogEntry @NewLogEntry -type 1 -entry "maxLogSize    = $maxLogSize"
+        New-CmnLogEntry @NewLogEntry -type 1 -entry "maxLogHistory = $maxLogHistory"
+        New-CmnLogEntry @NewLogEntry -type 1 -entry "writeOutput   = $writeOutput"
     }
 
     process {
-        New-CMNLogEntry -entry 'Beginning process loop' -type 1 @NewLogEntry
+        New-CmnLogEntry @NewLogEntry -type 1 -entry 'Beginning process loop'
         # Get each octet seperate
-        $octets = $subnetMask -split "\." 
+        $octets = $subnetMask -split '\.' 
         $subnetInBinary = @()
 
         # Let's convert the octets to binary
@@ -95,12 +116,12 @@ Function ConvertTo-CmnCidr {
             # Convert to binary 
             $octetInBinary = [convert]::ToString($octet, 2) 
             # Get length of binary string add leading zeros to make octet 
-            $octetInBinary = ("0" * (8 - ($octetInBinary).Length) + $octetInBinary) 
+            $octetInBinary = ('0' * (8 - ($octetInBinary).Length) + $octetInBinary) 
             # Add to variable
             $subnetInBinary = $subnetInBinary + $octetInBinary 
         } 
-        $subnetInBinary = $subnetInBinary -join "" 
-        New-CMNLogEntry -Entry "Subnet = $subnetInBinary" -type 1 @NewLogEntry
+        $subnetInBinary = $subnetInBinary -join '' 
+        New-CMNLogEntry @NewLogEntry -type 1 -Entry "Subnet = $subnetInBinary"
 
         # Now, let's make sure it's a valid subnet mask
         $x = 0
@@ -114,7 +135,7 @@ Function ConvertTo-CmnCidr {
             if ($subnetInBinary.Substring($x, 1) -ne '0') {
                 # No good, time to alert!
                 $errorMessage = "The mask $subnetMask is invalid"
-                New-CMNLogEntry -entry $errorMessage -type 3 @NewLogEntry
+                New-CMNLogEntry @NewLogEntry -type 3 -entry $errorMessage
                 throw $errorMessage
             } 
             $x++ 
@@ -122,10 +143,17 @@ Function ConvertTo-CmnCidr {
     }
 
     End {
+        New-CmnLogEntry @NewLogEntry -type 1 -entry 'Beginning end loop'
         # Done! Log and send back the results
         $results = "$ipAddress/$networkBits"
-        New-CMNLogEntry -entry "Returning $results" -type 1 @NewLogEntry
-        New-CMNLogEntry -entry 'Completing Function' -type 1 @NewLogEntry
-        Return $results
+        $obj = New-Object -TypeName PSObject -Property @{
+            CidrAddress = $results
+        }
+        $obj.PSObject.TypeNames.Insert(0, 'CMN.CiderAddress')
+        New-CMNLogEntry @NewLogEntry -type 1 -entry "Returning $obj"
+        New-CMNLogEntry @NewLogEntry -type 1 -entry 'Completing Function'
+        Return $obj
     }
-} #End ConvertTo-CmnCidr
+}
+
+ConvertTo-CmnCidr -ipAddress 192.168.1.25 -subnetMask 255.255.255.0
